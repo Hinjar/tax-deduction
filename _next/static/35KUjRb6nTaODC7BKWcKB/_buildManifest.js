2788403d50aaf69c9b2c86c4ee319d884d65d826
Hinjar <62614199+Hinjar@users.noreply.github.com>
2021-10-14 07:50:10 +0000

    Deploying to gh-pages from @ Hinjar/tax-deduction@3d11234b4787b237cd7164ca7e5fb6ee43d0b381 🚀

---
+++ _next/static/35KUjRb6nTaODC7BKWcKB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/56e579bbb2601a8de1a7.css","static/chunks/pages/index-bfb884ec901c4013b68b.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
